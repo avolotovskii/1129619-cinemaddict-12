acd6dda7a910eeab8f543e49d8040d23840033c3
Anatolii <volotovskiy111@gmail.com>
2020-08-28 15:07:47 +0300

    Делает сортировку

--- src/presenter/movie-list.js
+++ src/presenter/movie-list.js
@@ -1,9 +1,11 @@
 import FilmsCard from "../view/film-card";
 import FilmsList from "../view/films-list.js";
+import SortFilms, {SortType} from "../view/sort-films.js";
 import LoadMoreButton from "../view/load-more-button";
 import PopupFilmDetails from "../view/popup-film-details";
 import NoFilms from "../view/no-films.js";
 import {render, remove, RenderPosition} from "../utils/render.js";
+import {getDateFromString} from "../utils/common.js";
 
 const SHOWN_FILM_CARDS_AMOUNT = 5;
 const FILM_CARDS_AMOUNT_BY_BUTTON = 5;
@@ -57,48 +59,92 @@ const renderFilmCard = (filmsListElement, film) => {
   render(filmsListElement, filmCard, RenderPosition.BEFOREEND);
 };
 
+const renderFilmCards = (filmListElement, films) => {
+  films.forEach((film) => {
+    renderFilmCard(filmListElement, film);
+  });
+};
+
+const getSortedFilmCards = (films, sortType, from, to) => {
+  let sortedFilms = [];
+  const showFilms = films.slice();
+
+  switch (sortType) {
+    case SortType.DATE:
+      sortedFilms = showFilms.sort((a, b) => getDateFromString(b.releaseDate) - getDateFromString(a.releaseDate));
+      break;
+    case SortType.RATINNG:
+      sortedFilms = showFilms.sort((a, b) => b.rating - a.rating);
+      break;
+    case SortType.DEFAULT:
+      sortedFilms = showFilms;
+      break;
+  }
+
+  return sortedFilms.slice(from, to);
+};
+
 export default class MovieList {
   constructor(container) {
     this._container = container;
     this._filmsList = new FilmsList();
+    this._sortComponent = new SortFilms();
     this._loadMoreButton = new LoadMoreButton();
     this._noFilms = new NoFilms();
   }
 
   render(films) {
+    const renderShowMoreButton = () => {
+      if (shownFilmsCount >= films.length) {
+        return;
+      }
+
+      const filmsListElement = container.querySelector(`.films-list__container`);
+      render(filmsListElement, this._loadMoreButton, RenderPosition.AFTEREND);
+
+      const onShowMoreButtonClick = () => {
+        const prevFilmsCount = shownFilmsCount;
+        shownFilmsCount = shownFilmsCount + FILM_CARDS_AMOUNT_BY_BUTTON;
+
+        const sortedFilmCards = getSortedFilmCards(films, this._sortComponent.getSortType(), prevFilmsCount, shownFilmsCount);
+        renderFilmCards(filmsListElement, sortedFilmCards);
+
+        if (shownFilmsCount >= films.length) {
+          remove(this._loadMoreButton);
+        }
+      };
+
+      this._loadMoreButton.setClickHandler(() => {
+        onShowMoreButtonClick();
+      });
+    };
+
     const container = this._container.getElement();
 
     if (films.length === 0) {
       render(container, this._noFilms, RenderPosition.BEFOREEND);
       return;
     }
+    render(container, this._sortComponent, RenderPosition.BEFOREEND);
     render(container, this._filmsList, RenderPosition.BEFOREEND);
 
     const filmsListElement = container.querySelector(`.films-list__container`);
 
-    let shownFilmCardsAmount = SHOWN_FILM_CARDS_AMOUNT;
-    films.slice(0, shownFilmCardsAmount)
-      .forEach((filmCard) => {
-        renderFilmCard(filmsListElement, filmCard);
-      });
+    let shownFilmsCount = SHOWN_FILM_CARDS_AMOUNT;
+    renderFilmCards(filmsListElement, films.slice(0, shownFilmsCount));
+    renderShowMoreButton();
 
     render(container, this._loadMoreButton, RenderPosition.BEFOREEND);
 
-    const onShowMoreButtonClick = () => {
-      const prevFilmsCount = shownFilmCardsAmount;
-      shownFilmCardsAmount = shownFilmCardsAmount + FILM_CARDS_AMOUNT_BY_BUTTON;
+    this._sortComponent.setSortTypeChangeHandler((sortType) => {
+      shownFilmsCount = FILM_CARDS_AMOUNT_BY_BUTTON;
 
-      films.slice(prevFilmsCount, shownFilmCardsAmount)
-      .forEach((filmCard) => renderFilmCard(filmsListElement, filmCard));
+      const sortedFilmCards = getSortedFilmCards(films, sortType, 0, shownFilmsCount);
 
-      if (shownFilmCardsAmount >= films.length) {
-        remove(this._loadMoreButton);
-        this._loadMoreButton.removeElement();
-      }
-    };
+      filmsListElement.innerHTML = ``;
 
-    this._loadMoreButton.setClickHandler(() => {
-      onShowMoreButtonClick();
+      renderFilmCards(filmsListElement, sortedFilmCards);
+      renderShowMoreButton();
     });
   }
 }
--- src/view/sort-films.js
+++ src/view/sort-films.js
@@ -1,17 +1,61 @@
 import AbstractView from "./abstract.js";
 
+export const SortType = {
+  DATE: `date`,
+  RATING: `rating`,
+  DEFAULT: `default`,
+};
+
 const createSortFilms = () => {
   return (
     `<ul class="sort">
-      <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-      <li><a href="#" class="sort__button">Sort by date</a></li>
-      <li><a href="#" class="sort__button">Sort by rating</a></li>
+      <li><a href="#" data-sort-type="${SortType.DEFAULT}" class="sort__button sort__button--active">Sort by default</a></li>
+      <li><a href="#" data-sort-type="${SortType.DATE}" class="sort__button">Sort by date</a></li>
+      <li><a href="#" data-sort-type="${SortType.RATING}" class="sort__button">Sort by rating</a></li>
     </ul>`
   );
 };
 
 export default class SortFilms extends AbstractView {
+  constructor() {
+    super();
+
+    this._currenSortType = SortType.DEFAULT;
+  }
+
   getTemplate() {
     return createSortFilms();
+  }
+
+  getSortType() {
+    return this._currenSortType;
+  }
+
+  setSortTypeChangeHandler(handler) {
+    this.getElement().addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+
+      const siblingsElement = Array.from(evt.target.parentNode.parentNode.children);
+
+      if (evt.target.tagName !== `A`) {
+        return;
+      }
+
+      const sortType = evt.target.dataset.sortType;
+
+      if (this._currenSortType === sortType) {
+        return;
+      }
+
+      siblingsElement.forEach((el) => {
+        el.firstElementChild.classList.remove(`sort__button--active`);
+      });
+
+      this._currenSortType = sortType;
+
+      evt.target.classList.add(`sort__button--active`);
+
+      handler(this._currenSortType);
+    });
   }
 }
